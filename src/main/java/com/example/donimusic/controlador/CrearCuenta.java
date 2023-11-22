0b66dfa395d3e6a4ff203ce4cf3f00ddd13d263b
angeljpr6 <angelp.r806@gmail.com>
2023-11-22 13:44:37 +0000

    Solo la creacion de CrearCuenta

---
+++ src/main/java/com/example/donimusic/controlador/CrearCuenta.java
@@ -0,0 +1,13 @@
+package com.example.donimusic.controlador;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CrearCuenta implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
